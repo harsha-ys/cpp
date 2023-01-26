f78b46a6936b5798e0de583cd4a911e3e1483004
harsha-ys <hyoshith@gmail.com>
2023-01-26 13:25:23 +0530

    string stream

---
+++ 10_string_stream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+	// Complete this function
+    stringstream ss(str);
+    vector<int> integers;
+    int a;char ch;
+    while (ss>>a)
+    {
+        integers.push_back(a);
+        ss>>ch;
+    }
+    return integers;
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
